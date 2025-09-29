2fb3a3cf43ee9d66c5fa785e8d9526e79cdcd0c2
Omkark172 <omkarkollure9@gmail.com>
2025-09-29 21:50:10 +0530

    New Git Created

---
+++ Addition.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Addition {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter first Number :");
+        int a = s.nextInt();
+        System.out.println("Enter second Number :");
+        int b = s.nextInt();
+        System.out.println("Addition = "+(a+b));
+    }
+}
