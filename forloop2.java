096f7e2bbd523733a87b652bbc5deb1d8ce76af7
OmkarK <okollure@gmail.com>
2026-01-20 19:36:59 +0530

    Descending for loop

---
+++ forloop2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class forloop2{
+    public static void main(String [] args){
+        for(int i = 10; i >=0 ; i--){
+            System.out.println(i);
+        }
+
+    }
+}
